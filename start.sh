3708c9e3b53aa0d299cb577083469c9250b5520e
nkasande <79195467+nkasande@users.noreply.github.com>
2026-06-12 14:59:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-set -e
 
-for file in $(ls -1 *.sql | sort); do
-  echo "Running $file..."
-  psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d "$PGDATABASE" -f "$file" || exit 1
-done
-
-echo "All scripts completed successfully"
